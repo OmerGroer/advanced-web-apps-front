630ffddbf30e557bbaeab19e78f2b2a3da66b7cd
Omer Groer <drutr123@gmail.com>
2025-01-31 10:38:51 +0200

    rollback wrong push

--- src/components/Feed/Feed.tsx
+++ src/components/Feed/Feed.tsx
@@ -2,7 +2,7 @@ import { FC } from "react";
 import PostsList from "../PostsList/PostsList";
 
 const Feed: FC = () => {
-  return <PostsList withRecommendation={false} />;
+  return <PostsList withRecommendation />;
 };
 
 export default Feed;
